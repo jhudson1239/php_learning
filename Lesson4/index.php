74af028bcf623e5bc17c00c6302270cf8555ae53
Joshua <joshua.hudson@go.edgehill.ac.uk>
2018-06-28 14:19:44 +0100

    lesson4

---
+++ Lesson4/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <?php
+    //string
+    $foo = "String";
+
+    //integer
+    $ba = 20;
+
+    //float
+    $blah = 20.3243;
+
+    //boolean
+    true;
+    false;
+
+    //array
+
+    $names = array(
+        "Joshua",
+        "Bob",
+        "John"
+    );
+
+    echo $names['1'];
+
+
+    ?>
+</body>
+</html>
